cd05061c500f0262090ed7e0d0401f189c5674e8
Gabriele Fontana <gabriele.fontana@mondora.com>
2021-02-06 02:53:59 +0100

    fix: ftp passive mode

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 /*
 Plugin Name: FTP Webcam Image Downloader
-Description: 
+Description: Semplice downloader di file via ftp, Puoi trovare la configurazione nel menu impostazioni
+alla voce: Webcam Downloader Option
 Version:     1.0
 Author:      Gabriele Fontana <gafreax@gmail.com>
 License:     GPL3
@@ -25,92 +26,59 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 
-error_log('---  FTP Webcam Image Downloader ----  ');
+error_log('------  FTP Webcam Image Downloader ----  ');
+$msglog = 'FTP Webcam Downloader';
 function download_image()
 {
-    $server = get_option('webcam_downloader_ftp_server');
-    if (empty($server)) {
-        error_log('gaf - server not defined');
-        return;
-    }
-    $username = get_option('webcam_downloader_ftp_username');
-    if (empty($username)) {
-        error_log('gaf - username not defined');
-        return;
-    }
-    $password = get_option('webcam_downloader_ftp_password');
-    if (empty($password)) {
-        error_log('gaf - password not defined');
-        return;
-    }
-    $remotefile = get_option('webcam_downloader_ftp_remotefile');
-    if (empty($remotefile)) {
-        error_log('gaf - remotefile not defined');
-        return;
-    }
-    $localfile = get_option('webcam_downloader_ftp_localfile');
-    if (empty($localfile)) {
-        error_log('gaf - localfile not defined');
-        return;
-    }
-    $scheduling = get_option('webcam_downloader_ftp_scheduling');
-    if (empty($scheduling)) {
-        error_log('gaf - scheduling not defined');
-        return;
-    }
-    error_log("gaf - remote " . $remotefile);
-    error_log("gaf - local " . $localfile);
-    error_log("gaf - sched " . $scheduling);
     try {
-        $connection = ftp_connect($server);
+        $server = get_option('webcam_downloader_ftp_server');
+        $username = get_option('webcam_downloader_ftp_username');
+        $password = get_option('webcam_downloader_ftp_password');
+        $remotefile = get_option('webcam_downloader_ftp_remotefile');
+        $localfile = get_option('webcam_downloader_ftp_localfile');
+        $scheduling = get_option('webcam_downloader_ftp_scheduling');
 
-        error_log("gaf - connection " . $connection);
-        if ($connection !== false) {
-            $login = ftp_login($connection, $username, $password);
-            if (empty($login)) {
-                error_log("gaf - No login");
-                error_log("gaf - login " . $login);
+        if (empty($server) || empty($scheduling) ||empty($localfile)|| empty($remotefile)||empty($password)||empty($username)) {
+            throw new InvalidArgumentException('server not defined');
+        }
 
-                error_log("gaf - remote " . $remotefile);
-                error_log("gaf - username " . $username);
-                error_log("gaf - password " . $password);
-                error_log("gaf - local " . $localfile);
-                error_log("gaf - sched " . $scheduling);
+        $connection = ftp_connect($server, 21);
+        if (!$connection) {
+            throw new ErrorException('can not connect');
+        }
 
-                error_log("gaf - server " . $server);
-                return;
-            }
-            while (true && $login) {
-                $up = wp_upload_dir();
-                $ftpfile = ftp_get($connection,  $up['basedir']  . "/" .  $localfile, $remotefile, FTP_BINARY);
-                if ($ftpfile) {
-                    error_log("Successfully written to $localfile\n");
-                    if ($connection) {
-                        ftp_close($connection);
-                        sleep(60 * $scheduling);
-                    }
-                } else {
-                    error_log("gaf - No conn");
-                    error_log("gaf - basedir " . $up['basedir']);
-                    error_log("gaf - server " . $server);
-                    error_log("gaf - login " . $login);
-                    error_log("gaf - ftpfile " . $ftpfile ? 'ok' : 'ko');
-                    error_log("gaf - local " . $localfile);
-                    error_log("gaf - sched " . $scheduling);
-                }
-            }
-        } else {
-            error_log("gaf - No conn");
+        $login = ftp_login($connection, $username, $password);
+        if (!$login) {
+            throw new ErrorException('can not login with ' . $login);
+        }
+        ftp_pasv($connection, true);
+        $up = wp_upload_dir();
+        $ftpfile = ftp_get($connection, $up['basedir']  . "/" .  $localfile, $remotefile, FTP_BINARY);
+        if (!$ftpfile) {
+            throw new ErrorException('can not login with ' . $login);
+        }
+        error_log($msglog . 'Successfully written to '. $localfile);
+        copy($up['basedir']  . "/" .$localfile, $up['basedir']  . "/" . $localfile.'-'. date('H:i') . '.jpg');
+        if ($connection) {
+            ftp_close($connection);
         }
     } catch (exception $e) {
-        error_log("gaf - " . $e);
+        wp_clear_scheduled_hook('webcam_downloader_cron_hook');
+        $msg = $msglog . ' Exception - ';
+        error_log($msg . $e);
+        error_log($msg . $remotefile);
+        error_log($msg . $remotefile);
+        error_log($msg . $username);
+        error_log($msg . $password);
+        error_log($msg . $localfile);
+        error_log($msg . $scheduling);
+        error_log($msg . $server);
     }
-    return;
 }
 
 function myplugin_register_settings()
 {
-    error_log("gaf - Register Settings");
+    error_log($msglog .  '- Register Settings');
     add_option('webcam_downloader_ftp_server');
     register_setting('myplugin_options_group', 'webcam_downloader_ftp_server');
     add_option('webcam_downloader_ftp_username');
@@ -124,76 +92,90 @@ function myplugin_register_settings()
     add_option('webcam_downloader_ftp_scheduling');
     register_setting('myplugin_options_group', 'webcam_downloader_ftp_scheduling');
 }
-add_action('admin_init', 'myplugin_register_settings');
 
 function myplugin_register_options_page()
 {
-    error_log("gaf - Add option pages");
+    error_log($msglog . ' - Add option pages');
     add_options_page('Webcam Downloader', 'Webcam Downloader Option', 'manage_options', 'myplugin', 'myplugin_options_page');
 }
 
-
 function myplugin_options_page()
-{
-?>
+{?>
     <div>
         <?php screen_icon(); ?>
         <h2>Webcam Downloader</h2>
+        <p>
+        <img  height="200px" src="/wp-content/uploads/<?= get_option('webcam_downloader_ftp_localfile') ?>" alt="image downloaded" />
+        <br />
+        <?= '/wp-content/uploads/'. get_option('webcam_downloader_ftp_localfile') ?>
+         </p>
         <form method="post" action="options.php">
             <?php settings_fields('myplugin_options_group'); ?>
             <h3>Opzioni</h3>
             <table>
-                <tr valign="top">
+            <caption>Imposta i dati per accedere al server ftp della webcam</caption>
+                <tr >
                     <th scope="row"><label for="webcam_downloader_ftp_server">Server</label></th>
-                    <td><input type="text" id="webcam_downloader_ftp_server" name="webcam_downloader_ftp_server" value="<?php echo get_option('webcam_downloader_ftp_server'); ?>" /></td>
+                    <td><input type="text" id="webcam_downloader_ftp_server" name="webcam_downloader_ftp_server" value="<?= get_option('webcam_downloader_ftp_server') ?>" /></td>
                 </tr>
-                <tr valign="top">
+                <tr >
                     <th scope="row"><label for="webcam_downloader_ftp_username">Username</label></th>
-                    <td><input type="text" id="webcam_downloader_ftp_username" name="webcam_downloader_ftp_username" value="<?php echo get_option('webcam_downloader_ftp_username'); ?>" /></td>
+                    <td><input type="text" id="webcam_downloader_ftp_username" name="webcam_downloader_ftp_username" value="<?= get_option('webcam_downloader_ftp_username') ?>" /></td>
                 </tr>
-                <tr valign="top">
+                <tr >
                     <th scope="row"><label for="webcam_downloader_ftp_password">Password</label></th>
-                    <td><input type="password" id="webcam_downloader_ftp_password" name="webcam_downloader_ftp_password" value="<?php echo get_option('webcam_downloader_ftp_password'); ?>" /></td>
+                    <td><input type="password" id="webcam_downloader_ftp_password" name="webcam_downloader_ftp_password" value="<?= get_option('webcam_downloader_ftp_password') ?>" /></td>
                 </tr>
-                <tr valign="top">
+                <tr >
                     <th scope="row"><label for="webcam_downloader_ftp_remotefile">File Remoto</label></th>
-                    <td><input type="text" id="webcam_downloader_ftp_remotefile" name="webcam_downloader_ftp_remotefile" value="<?php echo get_option('webcam_downloader_ftp_remotefile'); ?>" /></td>
+                    <td><input type="text" id="webcam_downloader_ftp_remotefile" name="webcam_downloader_ftp_remotefile" value="<?= get_option('webcam_downloader_ftp_remotefile') ?>" /></td>
                 </tr>
-                <tr valign="top">
+                <tr >
                     <th scope="row"><label for="webcam_downloader_ftp_localfile">File locale</label></th>
-                    <td><img src="/wp-content/uploads/<?php echo get_option('webcam_downloader_ftp_localfile'); ?>" alt="image downloaded" /> <input type="text" id="webcam_downloader_ftp_localfile" name="webcam_downloader_ftp_localfile" value="<?php echo get_option('webcam_downloader_ftp_localfile'); ?>" /></td>
+                    <td>
+                          <input type="text" id="webcam_downloader_ftp_localfile" name="webcam_downloader_ftp_localfile" value="<?= get_option('webcam_downloader_ftp_localfile') ?>" />
+                    </td>
                 </tr>
-                <tr valign="top">
-                    <th scope="row"><label for="webcam_downloader_ftp_scheduling">Ogni quanto vuoi verificare la nuova immagine</label></th>
-                    <td><input type="text" id="webcam_downloader_ftp_scheduling" name="webcam_downloader_ftp_scheduling" value="<?php echo get_option('webcam_downloader_ftp_scheduling'); ?>" /></td>
+                <tr >
+                    <th scope="row"><label for="webcam_downloader_ftp_scheduling">Minuti per la verifica</label></th>
+                    <td><input type="text" id="webcam_downloader_ftp_scheduling" name="webcam_downloader_ftp_scheduling" value="<?= get_option('webcam_downloader_ftp_scheduling') ?>" /></td>
                 </tr>
+
             </table>
             <?php submit_button(); ?>
         </form>
+                   
     </div>
 <?php
 }
-add_action('admin_menu', 'myplugin_register_options_page');
-
-//add_action('plugins_loaded', 'download_image');
-add_action('added_option', 'download_image', 10, 2);
-add_action('updated_option', 'download_image', 10, 3);
-
 
 function webcam_add_cron_interval($schedules)
 {
     $scheduling = get_option('webcam_downloader_ftp_scheduling');
     if (empty($scheduling)) {
-        error_log('gaf - scheduling not defined');
+        error_log($msglog . ' - scheduling not defined');
         return $schedules;
     }
-    $schedules['webcam_downloader'] = array(
+    $schedules['webcam_downloader_schedules'] = array(
         'interval' => 60 * $scheduling,
         'display'  => esc_html__('Every Some times'),
     );
     return $schedules;
 }
 if (!wp_next_scheduled('webcam_downloader_cron_hook')) {
-    wp_schedule_event(time(), 'webcam_downloader_cron_hook', 'download_image');
+    wp_schedule_event(time(), 'webcam_downloader_schedules', 'download_image');
 }
-// add_filter('cron_schedules', 'webcam_add_cron_interval');
+
+register_deactivation_hook(__FILE__, 'bl_deactivate');
+ 
+function bl_deactivate()
+{
+    $timestamp = wp_next_scheduled('webcam_downloader_cron_hook');
+    wp_unschedule_event($timestamp, 'webcam_downloader_cron_hook');
+}
+
+add_action('admin_init', 'myplugin_register_settings');
+add_action('admin_menu', 'myplugin_register_options_page');
+add_action('added_option', 'download_image', 10, 2);
+add_action('updated_option', 'download_image', 10, 3);
+add_filter('cron_schedules', 'webcam_add_cron_interval');
